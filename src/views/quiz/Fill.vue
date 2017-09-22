e6458b59fc39f2e252af93bdde8cf6527da6985c
Demetrio Carrara <carrarademetrio@gmail.com>
2017-09-22 16:01:39 +0200

    Complete draft of FillIn view
    Signed-off-by: Demetrio Carrara <carrarademetrio@gmail.com>

--- src/views/quiz/Fill.vue
+++ src/views/quiz/Fill.vue
@@ -1,44 +1,62 @@
 <template>
-  <div class="layout-padding">
-    <q-card>
-      <q-card-title>
-        {{ quiz.questions[selected - 1].text }}
-      </q-card-title>
-      <q-card-separator />
-      <q-card-main>
-        <!-- Domanda a risposta singola -->
-        <div v-if="quiz.questions[selected - 1].solutionType === 'single'">
-          <div v-for="answer of quiz.questions[selected - 1].answers" :key="answer.id">
-            <q-radio v-model="answerId" :val="answer.id" :label="answer.text" />
-          </div>
-        </div>
+  <div>
+    <q-tabs slot="navigation">
+      <q-tab default slot="title" name="fill-in" icon="edit">Fill in</q-tab>
+      <q-tab slot="title" name="summary" icon="assignment">Summary</q-tab>
 
-        <!-- Domanda a risposta multipla -->
-        <div v-else-if="quiz.questions[selected - 1].solutionType === 'multiple'">
-          <div v-for="answer of quiz.questions[selected - 1].answers" :key="answer.id">
-            {{ answer.text }}
-          </div>
-        </div>
+      <q-tab-pane name="fill-in">
+        <div class="layout-padding">
+          <q-card>
+            <q-card-title>
+              {{ quiz.questions[selected - 1].text }}
+            </q-card-title>
+            <q-card-separator />
+            <q-card-main>
+              <!-- Domanda a risposta singola -->
+              <div v-if="quiz.questions[selected - 1].solutionType === 'single'">
+                <div v-for="answer of quiz.questions[selected - 1].answers" :key="answer.id">
+                  <q-radio v-model="answerId" :val="answer.id" :label="answer.text" />
+                </div>
+              </div>
 
-        <!-- Domanda a risposta aperta -->
-        <div v-else>
-          <!-- TODO: Insert textbox here -->
-        </div>
-      </q-card-main>
-      <q-card-actions align="center">
-        <q-pagination v-model="selected" :max="quiz.questions.length" />
-      </q-card-actions>
-    </q-card>
+              <!-- Domanda a risposta multipla -->
+              <div v-else-if="quiz.questions[selected - 1].solutionType === 'multiple'">
+                <div v-for="answer of quiz.questions[selected - 1].answers" :key="answer.id">
+                  <q-checkbox v-model="answersIds" :val="answer.id" :label="answer.text"/>
+                </div>
+              </div>
 
-    <q-fixed-position corner="bottom-right" :offset="[18, 18]">
-      <q-btn round color="primary" @click="submitQuiz" icon="send" />
-    </q-fixed-position>
+              <!-- Domanda a risposta aperta -->
+              <div v-else>
+                <q-input v-model="openAnswerText" />
+              </div>
+            </q-card-main>
+          </q-card>
+
+
+          <q-fixed-position class="fixed-bottom" :offset="[18, 18]">
+            <q-card>
+              <q-pagination class="justify-center" style="display:flex" v-model="selected" :max="quiz.questions.length" />
+            </q-card>
+          </q-fixed-position>
+        </div>
+      </q-tab-pane>
+
+      <q-tab-pane name="summary">
+        <q-fixed-position corner="bottom-right" :offset="[18, 18]">
+          <q-btn color="primary" @click="submitQuiz">Check your answers</q-btn>
+        </q-fixed-position>
+      </q-tab-pane>
+    </q-tabs>
+
+
   </div>
+
 </template>
 
 <script>
 import { mapState } from "vuex"
-import { QCard, QCardTitle, QCardSeparator, QCardMain, QCardActions, QPagination, QRadio, QFixedPosition, QBtn } from "quasar"
+import { QCard, QCardTitle, QCardSeparator, QCardMain, QPagination, QRadio, QCheckbox, QInput, QFixedPosition, QBtn, QTabs, QTab, QTabPane } from "quasar"
 
 export default {
   components: {
@@ -46,47 +64,52 @@ export default {
     QCardTitle,
     QCardMain,
     QCardSeparator,
-    QCardActions,
     QPagination,
     QRadio,
+    QCheckbox,
+    QInput,
     QFixedPosition,
-    QBtn
+    QBtn,
+    QTabs,
+    QTab,
+    QTabPane
   },
   data () {
     return {
-      selected: 1
+      selected: 1,
+      answerId: null,
+      answersIds: [],
+      openAnswerText: null
     }
   },
   computed: {
     ...mapState({
       quiz: state => state.currentQuiz
-    }),
-    // Two-way data-binding between Vuex and UI
-    answersIds: {
-      get () {
-        return this.$store.state.quizSubmission.questions[this.selected - 1].answersIds
-      },
-      set (value) {
-        this.$store.commit("updateAnswersIds", { index: this.selected - 1, answersIds: value })
-      }
+      // I have to try this when I have a working example
+      // submission: state => state.quizSubmission
+    })
+  },
+  watch: {
+    // NOTE: These changes have to be done with computed properties but I can't figure out
+    // why it is not working with computed getter/setter retrieving values from store
+
+    // Update Vuex and QRadio v-model value
+    answerId: function (value) {
+      this.$store.commit("updateAnswerId", { index: this.selected - 1, answerId: value })
     },
-    answerId: {
-      get () {
-        let value = this.$store.state.quizSubmission.questions[this.selected - 1].answerId
-        console.log(value)
-        return value
-      },
-      set (value) {
-        this.$store.commit("updateAnswerId", { index: this.selected - 1, answerId: value })
-      }
+    // Update Vuex and QCheckbox v-model value
+    answersIds: function (value) {
+      this.$store.commit("updateAnswersIds", { index: this.selected - 1, answersIds: value })
     },
-    openAnswerText: {
-      get () {
-        return this.$store.state.quizSubmission.questions[this.selected - 1].openAnswerText
-      },
-      set (value) {
-        this.$store.commit("updateOpenAnswerText", { index: this.selected - 1, openAnswerText: value })
-      }
+    // Update Vuex and QInput v-model value
+    openAnswerText: function (value) {
+      this.$store.commit("updateOpenAnswerText", { index: this.selected - 1, openAnswerText: value })
+    },
+    // Update QRadio, QCheckbox or QInput when I change question
+    selected: function (value) {
+      this.answerId = this.$store.state.quizSubmission.questions[this.selected - 1].answerId
+      this.answersIds = this.$store.state.quizSubmission.questions[this.selected - 1].answersIds
+      this.openAnswerText = this.$store.state.quizSubmission.questions[this.selected - 1].openAnswerText
     }
   },
   methods: {
