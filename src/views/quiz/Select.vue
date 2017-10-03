aae241cdb61f98d13501527a62e41dd5b98ce3b0
Demetrio Carrara <carrarademetrio@gmail.com>
2017-10-03 11:56:48 +0200

    Change user interaction with Select view, prepare for course named quizzes
    Signed-off-by: Demetrio Carrara <carrarademetrio@gmail.com>

--- src/views/quiz/Select.vue
+++ src/views/quiz/Select.vue
@@ -1,40 +1,45 @@
 <template>
   <div class="layout-padding">
-    <div v-if="premadeQuizzes.length > 0">
-      <h5 class="text-center">Start a pre-made quiz</h5>
-      <div class="row inline" v-for="quiz of premadeQuizzes" :key="quiz.id">
-        <q-card inline>
-          <q-card-title>
-            {{ quiz.name }}
-          </q-card-title>
-        </q-card>
+    <p class="caption">Select a course</p>
+    <q-dialog-select
+      v-model="courseId"
+      :options="selectCourses"
+      title="Courses"
+      @change="updatePremadeQuizzes"
+    />
+    <div v-if="courseId.length !== 0">
+      <div v-if="premadeQuizzes.length > 0">
+        <h5 class="text-center">Start a pre-made quiz</h5>
+        <div class="row inline" v-for="quiz of premadeQuizzes" :key="quiz.id">
+          <q-card inline>
+            <q-card-title>
+              {{ quiz.name }}
+            </q-card-title>
+          </q-card>
+        </div>
       </div>
+      <q-card>
+        <q-card-title>
+          <h5 class="text-center">Create a random quiz</h5>
+        </q-card-title>
+        <q-card-main>
+          <p class="caption">{{ numberOfQuestions }} questions selected</p>
+          <q-slider v-model="numberOfQuestions" label :min="5" :max="20" />
+
+        </q-card-main>
+        <q-card-separator />
+        <q-card-actions align="end">
+          <q-btn
+            color="primary"
+            :disable="courseId.length === 0"
+            icon="create"
+            @click="startQuiz">
+            Let's go!
+          </q-btn>
+        </q-card-actions>
+      </q-card>
     </div>
-    <q-card>
-      <q-card-title>
-        <h5 class="text-center">Create a random quiz</h5>
-      </q-card-title>
-      <q-card-main>
-        <p class="caption">{{ numberOfQuestions }} questions selected</p>
-        <q-slider v-model="numberOfQuestions" label :min="5" :max="20" />
-        <p class="caption">Select a course</p>
-        <q-dialog-select
-          v-model="courseId"
-          :options="selectCourses"
-          title="Courses"
-        />
-      </q-card-main>
-      <q-card-separator />
-      <q-card-actions align="end">
-        <q-btn
-          color="primary"
-          :disable="courseId.length === 0"
-          icon="create"
-          @click="startQuiz">
-          Let's go!
-        </q-btn>
-      </q-card-actions>
-    </q-card>
+
   </div>
 </template>
 
@@ -80,11 +85,11 @@ export default {
     }).catch(function (data) {
       console.log(data)
     })
-    this.fetchQuizzes().then(data => {
+    /* this.fetchQuizzes().then(data => {
       this.$store.commit("updatePremadeQuizzes", { quizzes: data.data.data })
     }).catch(function (data) {
       console.log(data)
-    })
+    }) */
   },
   methods: {
     fetchCourses () {
@@ -92,6 +97,12 @@ export default {
     },
     fetchQuizzes () {
       return this.$http.get(this.$store.state.hostname + "quizzes/named")
+    },
+    fetchQuizzesByCourse (course) {
+      return this.$http.get(this.$store.state.hostname + "quizzes/named" /* ,
+        params: {
+          courseId: course
+        } */)
     },
     startQuiz () {
       this.$http.post(this.$store.state.hostname + "quizzes/create", {
@@ -102,6 +113,13 @@ export default {
         this.$store.commit("prepareQuizSubmission", { quiz: data.data.data })
         // Now I have to change view
         this.$router.push("/quiz/" + data.data.data.id)
+      }).catch(data => {
+        console.log(data)
+      })
+    },
+    updatePremadeQuizzes (selectedCourse) {
+      this.fetchQuizzesByCourse(selectedCourse).then(data => {
+        this.$store.commit("updatePremadeQuizzes", { quizzes: data.data.data })
       }).catch(data => {
         console.log(data)
       })
